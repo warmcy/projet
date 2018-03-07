a23c9db302d0f337c407dc03b3744212f6caf42e
hcylia <hcylia@hotmail.fr>
2018-03-07 13:08:49 -0500

    travis2

---
+++ ubuntu.sh
@@ -0,0 +1,22 @@
+--2018-03-07 13:04:44--  https://toolbelt.heroku.com/
+Resolving toolbelt.heroku.com (toolbelt.heroku.com)... 54.225.166.120, 50.19.119.234, 184.72.238.185
+Connecting to toolbelt.heroku.com (toolbelt.heroku.com)|54.225.166.120|:443... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://devcenter.heroku.com/articles/heroku-command-line [following]
+--2018-03-07 13:04:44--  https://devcenter.heroku.com/articles/heroku-command-line
+Resolving devcenter.heroku.com (devcenter.heroku.com)... 50.19.119.234, 184.72.238.185, 54.225.166.120
+Connecting to devcenter.heroku.com (devcenter.heroku.com)|50.19.119.234|:443... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://devcenter.heroku.com/articles/heroku-cli [following]
+--2018-03-07 13:04:44--  https://devcenter.heroku.com/articles/heroku-cli
+Reusing existing connection to devcenter.heroku.com:443.
+HTTP request sent, awaiting response... 200 OK
+Length: 125112 (122K) [text/html]
+Saving to: 'index.html'
+
+     0K .......... .......... .......... .......... .......... 40% 3.83M 0s
+    50K .......... .......... .......... .......... .......... 81% 7.26M 0s
+   100K .......... .......... ..                              100% 63.8M=0.02s
+
+2018-03-07 13:04:44 (6.02 MB/s) - 'index.html' saved [125112/125112]
+
