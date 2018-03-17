d18566cbe649de810fa62862ee6b4c90cb6db3e3
hcylia <hcylia@hotmail.fr>
2018-03-17 08:44:15 -0400

    commit temp

--- app/models/town.rb
+++ app/models/town.rb
@@ -11,17 +11,4 @@ class Town < ActiveRecord::Base
       self.long = current_town.lon
     end
   end
-  darksky = Darksky::API.new('9dd5cfea95cc580c3d6d1bd573594d73')
-  # Returns a forecast for the next hour at a given location.
-  forecast = darksky.forecast('42.7243','-73.6927')
-
-  # Returns a brief forecast for the next hour at a given location.
-  brief_forecast = darksky.brief_forecast('42.7243','-73.6927')
-
-  # Returns forecasts for a collection of arbitrary points.
-  precipitation = darksky.precipitation(['42.7','-73.6',1325607100,'42.0','-73.0',1325607791])
-
-  # Returns a list of interesting storms happening right now.
-  interesting_storms = darksky.interesting
-  
 end
